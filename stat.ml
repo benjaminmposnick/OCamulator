910b7cf1ce44f7c3605d1190064d2a698de0224c
Stephen <st624@cornell.edu>
2020-12-19 18:30:08 -0500

    added stats test

--- stat.ml
+++ stat.ml
@@ -8,7 +8,7 @@ let cum_sum (data : float list) =
   List.fold_left (fun acc x -> acc +. x) 0. data
 
 let cum_prod (data : float list) =
-  List.fold_left (fun acc x -> acc *. x) 0. data
+  List.fold_left (fun acc x -> acc *. x) 1. data
 
 let mean (data : float list) =
   let n = List.length data in
@@ -48,20 +48,43 @@ let median (data : float list) =
   else if n = 1 then List.nth data 0
   else median_val data (float_of_int n)
 
+let count (v : float) (data : float list) =
+  let rec count_helper v acc = function
+    | [] -> acc
+    | h::t ->
+      begin 
+        if h = v
+        then count_helper v (acc +. 1.) t
+        else count_helper v acc t
+      end
+  in count_helper v 0. data
+
+let rec mode_assoc acc lst =
+  match lst with 
+  | [] -> acc
+  | h::t -> 
+    begin 
+      if List.mem_assoc h acc
+      then mode_assoc acc t 
+      else mode_assoc ((h,count h lst)::acc) t
+    end
+
 (** from rosetta code*)
 let mode (data : float list) =
-  let seen = Hashtbl.create 42 in
-  List.iter (fun x ->
-      let old = if Hashtbl.mem seen x then
-          Hashtbl.find seen x
-        else 0 in
-      Hashtbl.replace seen x (old + 1))
-    data;
-  let best = Hashtbl.fold (fun _ -> max) seen 0 in
-  Hashtbl.fold (fun k v acc ->
-      if v = best then k :: acc
-      else acc)
-    seen [] 
+  let counts = mode_assoc [] data in
+  let rec max_value acc counts = 
+    match counts with 
+    | [] -> acc
+    | (k,v)::t -> 
+      begin 
+        if List.assoc k counts > acc
+        then max_value k t
+        else max_value acc t
+      end
+  in 
+  match data with
+  |[] -> 0.
+  |h::t -> max_value h counts
 
 let get_slope (x : float list) (y : float list) (mu_x : float) (mu_y : float) =
   mean (List.map2 (fun a b -> (a -. mu_x) *. (b -. mu_y)) x y) 
@@ -82,10 +105,14 @@ let quantile_helper (q : float) (n : int) =
   floor (q *. (n + 1 |> float_of_int))
 
 let quantile (data : float list) (q : float) =
-  List.length data 
-  |> quantile_helper q
-  |> int_of_float
-  |> List.nth (sort_asc data)
+  match data with
+  | [] -> 0.
+  | x::[] -> x
+  | h::t -> 
+    List.length data 
+    |> quantile_helper q
+    |> int_of_float
+    |> List.nth (sort_asc data)
 
 let rec max_helper = function
   | [] -> 0.
@@ -104,22 +131,15 @@ let min (data : float list) =
   let data = sort_asc data in
   min_helper data
 
+let range (data : float list) = 
+  max data -. min data
+
 let smpl_var (data : float list) =
-  mean_squared (err data)
+  let n = List.length data |> float_of_int in
+  (mean_squared (err data)) *. n /. (n -. 1.)
 
 let smpl_std (data : float list) =
   smpl_var data ** (0.5)
-
-let count (v : float) (data : float list) =
-  let rec count_helper v acc = function
-    | [] -> acc
-    | h::t ->
-      begin 
-        if h = v
-        then count_helper v (acc +. 1.) t
-        else count_helper v acc t
-      end
-  in count_helper v 0. data
 
 let unique (data : float list) =
   let rec unique_helper acc  = function
